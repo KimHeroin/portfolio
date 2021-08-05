cf01cc6bddb3dd492db39c025e4bc82a42d6aa1b
KimHeroin <yebboona@snu.ac.kr>
2021-08-05 10:08:32 +0900

    c

--- firstpage.js
+++ firstpage.js
@@ -1,18 +1,22 @@
-let angle=1;
+let angle=0;
 let img;
 function preload(){
-  img=loadImage("images/me.jpg");
+  img = loadImage("images/me.jpg")
 }
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  textureWrap(CLAMP);
 }
-
-function draw(){
-  pointLight(230, 230, 230, mouseX-400, mouseY-400, 0 );
+function draw() {
+  pointLight(255, 255, 255, mouseX-width, mouseY-height, 500);
   background(255);
   normalMaterial();
   texture(img);
-  box(200);
-  rotate(angle, angle, angle);
-  angle+=1;
+  rotateX(angle+(mouseX)*0.0008);
+  rotateY(angle+(mouseY)*0.0008);
+  rotateZ(angle+(mouseX+mouseY)*0.0008);
+  noStroke();
+  box(300);
+  sphere()
+  angle+=0.005;
 }
