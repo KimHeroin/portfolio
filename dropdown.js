a64f971d00f42bca1a2b45bf04f344881305126c
KimHeroin <yebboona@snu.ac.kr>
2021-08-08 03:48:54 +0900

    c

--- dropdown.js
+++ dropdown.js
@@ -4,13 +4,13 @@ function myFunction() {
 
 window.onclick = function(event) {
   if (!event.target.matches('.contact')) {
-    var dropdowns = document.getElementsByClassName("reachme_context");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+    //var dropdowns = document.getElementsByClassName("reachme_context");
+    //var i;
+  //  for (i = 0; i < dropdowns.length; i++) {
+    //  var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
   }
-}
+//}
