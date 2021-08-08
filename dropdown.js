610b2f8fc9360717431a0c052cd1fc360162e415
Kim Heroin <74931657+KimHeroin@users.noreply.github.com>
2021-08-08 13:52:25 +0900

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -4,7 +4,7 @@ function myFunction() {
 
 window.onclick = function(event) {
   if (!event.target.matches('.contact')) {
-    var dropdowns = document.getElementsByClassName("reachme_context");
+    var dropdowns = document.getElementsByClassName("reachme_text");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
