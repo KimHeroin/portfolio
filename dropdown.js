830a5c789ef7756fe640ea29080ddfc30902bbce
Kim Heroin <74931657+KimHeroin@users.noreply.github.com>
2021-08-08 14:03:11 +0900

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -3,7 +3,7 @@ function myFunction() {
 }
 
 window.onclick = function(event) {
-  if (!event.target.matches('.contact')) {
+  if (!event.target.matches('.reachme_text')) {
     var dropdowns = document.getElementsByClassName("reachme_text");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
